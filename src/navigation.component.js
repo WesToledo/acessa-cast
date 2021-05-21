59766b618f91579b0371c046c49005ffa2c895ed
Wesley Toledo <wesley.toledo.hexatech@gmail.com>
2021-05-21 11:12:04 -0300

    setup bottom navigator

--- src/navigation.component.js
+++ src/navigation.component.js
@@ -1,21 +1,62 @@
 import React from "react";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  BottomNavigation,
+  BottomNavigationTab,
+  Layout,
+  Text,
+  Icon,
+} from "@ui-kitten/components";
 
-import { HomeScreen } from "screens/home.component";
-import { DetailsScreen } from "screens/details.component";
+const { Navigator, Screen } = createBottomTabNavigator();
 
-const { Navigator, Screen } = createStackNavigator();
+const PersonIcon = (props) => <Icon {...props} name="person-outline" />;
 
-const HomeNavigator = () => (
-  <Navigator headerMode="none">
-    <Screen name="Home" component={HomeScreen} />
-    <Screen name="Details" component={DetailsScreen} />
+const BellIcon = (props) => <Icon {...props} name="bell-outline" />;
+
+const EmailIcon = (props) => <Icon {...props} name="email-outline" />;
+
+const UsersScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text category="h1">USERS</Text>
+  </Layout>
+);
+
+const OrdersScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text category="h1">ORDERS</Text>
+  </Layout>
+);
+
+const BottomTabBar = ({ navigation, state }) => (
+  <BottomNavigation
+    selectedIndex={state.index}
+    onSelect={(index) => navigation.navigate(state.routeNames[index])}
+  >
+    <BottomNavigationTab title="Home" icon={PersonIcon} />
+    <BottomNavigationTab title="Avaliações Físicas" icon={BellIcon} />
+    <BottomNavigationTab title="Order" icon={EmailIcon} />
+  </BottomNavigation>
+);
+
+const TabNavigator = () => (
+  <Navigator tabBar={(props) => <BottomTabBar {...props} />}>
+    <Screen name="Users" component={UsersScreen} />
+    <Screen name="Orders" component={OrdersScreen} />
+    <Screen name="Email" component={UsersScreen} />
   </Navigator>
 );
 
 export const AppNavigator = () => (
   <NavigationContainer>
-    <HomeNavigator />
+    <TabNavigator />
   </NavigationContainer>
 );
+
+const styles = StyleSheet.create({
+  bottomNavigation: {
+    marginVertical: 8,
+  },
+});
